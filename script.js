763d0ffe75e1fb23d6e05b7e0e8a4652d22acc0c
Alyubomirov1995 <Profffundo@gmail.com>
2024-01-31 11:28:18 +0300

    feat:Реализованный проект по таймеру

--- script.js
+++ script.js
@@ -1,42 +1,105 @@
-// на сколько минут ставим таймер
-var count = 25;
-// запущен таймер или нет
-started = false;
+let focusButton = document.getElementById("focus");
+let buttons = document.querySelectorAll(".btn");
+let shortBreakButton = document.getElementById("shortbreak");
+let longBreakButton = document.getElementById("longbreak");
+let startBtn = document.getElementById("btn-start");
+let reset = document.getElementById("btn-reset");
+let pause = document.getElementById("btn-pause");
+let time = document.getElementById("time");
+let set;
+let active = "focus";
+let count = 59;
+let paused = true;
+let minCount = 24;
+time.textContent = `${minCount + 1}:00`;
+const appendZero = (value) => {
+  value = value < 10 ? `0${value}` : value;
+  return value;
+};
+reset.addEventListener(
+  "click",
+  (resetTime = () => {
+    pauseTimer();
+    switch (active) {
+      case "long":
+        minCount = 14;
+        break;
+      case "short":
+        minCount = 4;
+        break;
+      default:
+        minCount = 24;
+        break;
+    }
+    count = 59;
+    time.textContent = `${minCount + 1}:00`;
+  })
+);
+const removeFocus = () => {
+  buttons.forEach((btn) => {
+    btn.classList.remove("btn-focus");
+  });
+};
 
-// запуск таймера по кнопке
-function start() {
-  // если таймер уже запущен — выходим из функции
-  if (started) {return};
-  // запоминаем время нажатия
-  var start_time = new Date(); 
-  // получаем время окончания таймера
-  var stop_time = start_time.setMinutes(start_time.getMinutes() + count); 
-
-  // запускаем ежесекундный отсчёт
-  var countdown = setInterval(function() {
-    // текущее время
-    var now = new Date().getTime();
-    // сколько времени осталось до конца таймера
-    var remain = stop_time - now; 
-    // переводим миллисекунды в минуты и секунды
-    var min = Math.floor( (remain % (1000 * 60 * 60)) / (1000 * 60) );
-    var sec = Math.floor( (remain % (1000 * 60)) / 1000 );
-    // если значение текущей секунды меньше 10, добавляем вначале ведущий ноль
-    sec = sec < 10 ? "0" + sec : sec;
-    // отправляем значение таймера на страницу в нужный раздел
-    document.getElementById("timer").innerHTML = min + ":" + sec;
-    // если время вышло
-    if (remain < 0) {
-      // останавливаем отсчёт
-      clearInterval(countdown);
-      // пишем текст вместо цифр
-      document.getElementById("timer").innerHTML = "Всё!";
-     }
-  }, 1000);
-  // помечаем, что таймер уже запущен
-  started = true;
-}
-
-function buttonClick(elem) {
-   elem.value = 'Стоп';
-}
+focusButton.addEventListener("click", () => {
+  document.body.style.backgroundColor = 'rgb(214, 96, 96)';
+  removeFocus();
+  focusButton.classList.add("btn-focus");
+  pauseTimer();
+  minCount = 24;
+  count = 59;
+  time.textContent = `${minCount + 1}:00`;
+});
+shortBreakButton.addEventListener("click", () => {
+  document.body.style.backgroundColor = '#00008b';
+  active = "short";
+  removeFocus();
+  shortBreakButton.classList.add("btn-focus");
+  pauseTimer();
+  minCount = 4;
+  count = 59;
+  time.textContent = `${appendZero(minCount + 1)}:00`;
+  
+});
+longBreakButton.addEventListener("click", () => {
+  document.body.style.backgroundColor = 'green';
+  active = "long";
+  removeFocus();
+  longBreakButton.classList.add("btn-focus");
+  pauseTimer();
+  minCount = 14;
+  count = 59;
+  time.textContent = `${minCount + 1}:00`;
+});
+pause.addEventListener(
+  "click",
+  (pauseTimer = () => {
+    paused = true;
+    clearInterval(set);
+    startBtn.classList.remove("hide");
+    pause.classList.remove("show");
+    reset.classList.remove("show");
+  })
+);
+startBtn.addEventListener("click", () => {
+  reset.classList.add("show");
+  pause.classList.add("show");
+  startBtn.classList.add("hide");
+  startBtn.classList.remove("show");
+  if (paused) {
+    paused = false;
+    time.textContent = `${appendZero(minCount)}:${appendZero(count)}`;
+    set = setInterval(() => {
+      count--;
+      time.textContent = `${appendZero(minCount)}:${appendZero(count)}`;
+      if (count == 0) {
+        if (minCount != 0) {
+          minCount--;
+          count = 60;
+        } else {
+          clearInterval(set);
+        }
+      }
+    }, 1000);
+  }
+});
